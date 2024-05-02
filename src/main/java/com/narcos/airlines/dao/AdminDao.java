c9241a2cfdabace8d27e035481ee7df20ecc4056
omarebid1 <omar3bid18@gmail.com>
2024-05-03 01:50:37 +0300

    Admin & Crew Dao's Done

--- src/main/java/com/narcos/airlines/dao/AdminDao.java
+++ src/main/java/com/narcos/airlines/dao/AdminDao.java
@@ -1,4 +1,14 @@
 package com.narcos.airlines.dao;
 
+import com.narcos.airlines.repository.AdminRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdminDao {
+
+    @Autowired
+    private AdminRepo adminRepo;
+
+
 }
--- src/main/java/com/narcos/airlines/dao/CrewDao.java
+++ src/main/java/com/narcos/airlines/dao/CrewDao.java
@@ -1,4 +1,13 @@
 package com.narcos.airlines.dao;
 
+import com.narcos.airlines.repository.CrewRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CrewDao {
+
+    @Autowired
+    private CrewRepo crewRepo;
+
 }
