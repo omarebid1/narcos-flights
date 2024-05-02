ee35b363589eab351683b3e8f36988485fa83fa6
omarebid1 <omar3bid18@gmail.com>
2024-05-03 01:56:24 +0300

    Admin & Crew Controllers Done

--- src/main/java/com/narcos/airlines/controller/AdminController.java
+++ src/main/java/com/narcos/airlines/controller/AdminController.java
@@ -1,4 +1,20 @@
 package com.narcos.airlines.controller;
 
+
+import com.narcos.airlines.dao.AdminDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(path = "/admin")
+@RestController
+
+@CrossOrigin
 public class AdminController {
+
+    @Autowired
+    private AdminDao adminDao;
+
+
 }
--- src/main/java/com/narcos/airlines/controller/CrewController.java
+++ src/main/java/com/narcos/airlines/controller/CrewController.java
@@ -1,4 +1,18 @@
 package com.narcos.airlines.controller;
 
+import com.narcos.airlines.dao.CrewDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(path = "/crew")
+@RestController
+
+@CrossOrigin
 public class CrewController {
+
+    @Autowired
+    private CrewDao crewDao;
+
 }
