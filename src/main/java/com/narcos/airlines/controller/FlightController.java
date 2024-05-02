de5a6e4a68668c9116657ecb0294dd74eb46d9e5
omarebid1 <omar3bid18@gmail.com>
2024-05-03 02:33:37 +0300

    latest updates

--- src/main/java/com/narcos/airlines/controller/FlightController.java
+++ src/main/java/com/narcos/airlines/controller/FlightController.java
@@ -3,17 +3,23 @@ package com.narcos.airlines.controller;
 import com.narcos.airlines.dao.FlightDao;
 import com.narcos.airlines.entity.FlightEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequestMapping(path = "/flights")
 @RestController
+
+@CrossOrigin
 public class FlightController {
     @Autowired
     FlightDao flightDao;
-    @GetMapping(path = "/getFlights")
-    public List<FlightEntity> getUsers(){
+
+    @GetMapping(path = "/get-all-flights")
+    public List<FlightEntity> getAllFlights() {
         return this.flightDao.getAllFlights();
     }
 }
--- src/main/java/com/narcos/airlines/controller/PassengerController.java
+++ src/main/java/com/narcos/airlines/controller/PassengerController.java
@@ -3,18 +3,23 @@ package com.narcos.airlines.controller;
 import com.narcos.airlines.dao.PassangerDao;
 import com.narcos.airlines.entity.PassengerEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequestMapping(path = "/passengers")
 @RestController
-public class PassangerController {
+
+@CrossOrigin
+public class PassengerController {
     @Autowired
     PassangerDao passangerDao;
 
-    @GetMapping(path = "/getAllPassangers")
-    List<PassengerEntity> getAllPassangers(){
+    @GetMapping(path = "/get-all-passengers")
+    List<PassengerEntity> getAllPassengers() {
         return this.passangerDao.getAllPassanger();
     }
 }
