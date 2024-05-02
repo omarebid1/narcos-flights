ba9100fc12ef3068c05af2963e17acdd8bc17c1c
xmaherr <mahernabil219@gmail.com>
2024-05-03 01:54:04 +0300

    create PassengerDao and FlightDao

--- src/main/java/com/narcos/airlines/dao/FlightDao.java
+++ src/main/java/com/narcos/airlines/dao/FlightDao.java
@@ -1,4 +1,12 @@
 package com.narcos.airlines.dao;
 
+import com.narcos.airlines.repository.FlightRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FlightDao {
+    @Autowired
+    private FlightRepo flightRepo;
+
 }
--- src/main/java/com/narcos/airlines/dao/PassangerDao.java
+++ src/main/java/com/narcos/airlines/dao/PassangerDao.java
@@ -1,4 +1,11 @@
 package com.narcos.airlines.dao;
 
+import com.narcos.airlines.repository.PassengerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PassangerDao {
+    @Autowired
+    private PassengerRepo passengerRepo;
 }
